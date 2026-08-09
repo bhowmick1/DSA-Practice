851096578780769b43062411b6e52ef6efc53558
bhowmick1 <bhowmick1001@gmail.com>
2026-08-10 00:28:19 +0530

    add logic building ex 4 to the solution

---
+++ Logic_building/04_LargestDigit.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int largest = 0;
+
+    while(n > 0) {
+        int digit = n % 10;
+
+        if(digit > largest) {
+            largest = digit;
+        }
+
+        n = n / 10;
+    }
+
+    cout << largest;
+
+    return 0;
+}
